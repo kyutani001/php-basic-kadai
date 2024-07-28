9b80ada61c780fc7b25713ffe95e0df193d973dc
kyutani001 <kazu.yutani001@gmail.com>
2024-07-28 21:15:36 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+  <body>
+    <p>
+      <?php
+      $content = ['name' => 'onion', 'price' => '200' , 'weight' => '160'];
+      print_r($content);
+      ?>
+    </p>
+  </body>
+</html>
