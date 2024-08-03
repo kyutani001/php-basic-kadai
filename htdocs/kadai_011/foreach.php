c926fd44908b66ba92a9013001eef3208ef181ec
kyutani001 <kazu.yutani001@gmail.com>
2024-08-03 10:50:19 +0900

    Add files via upload

---
+++ htdocs/kadai_011/foreach.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+  <body>
+  <p>
+    <?php
+    $personal_data=['名前'=>'玉ねぎ','値段'=>200,'産地'=>'北海道'];
+
+    foreach($personal_data as $key => $value){
+      echo "{$key}:{$value}<br>";
+    }
+    ?>
+  </p>
+  </body>
+</html>
